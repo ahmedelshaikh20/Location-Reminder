3760e876ca2b6ed35c204210480b63fa4ecb9765
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-11-08 03:33:33 +0100

    implementing navigation with type safe navigation

--- app/src/main/java/com/example/locationreminder/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/locationreminder/navigation/AppNavigation.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.navigation.toRoute
 import com.example.locationreminder.locationreminder.presentation.mapscreen.MapBoxScreen
 import com.example.locationreminder.locationreminder.presentation.mapscreen.MapBoxScreenViewmodel
 import com.example.locationreminder.locationreminder.presentation.reminderlist.ReminderListScreen
@@ -13,32 +14,19 @@ import com.example.locationreminder.locationreminder.presentation.reminderlist.R
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun AppNavigation(mapBoxScreenViewmodel: MapBoxScreenViewmodel = koinViewModel(),
-                  reminderListScreenViewmodel: ReminderListScreenViewmodel= koinViewModel()
-                  ) {
+fun AppNavigation() {
   val navController = rememberNavController()
   NavHost(
     navController = navController,
-    startDestination = NavRoutes.LIST_SCREEN,
+    startDestination = ReminderListScreen,
   ) {
-
-    composable(NavRoutes.LIST_SCREEN) {
-      ReminderListScreen(reminderListScreenViewmodel,navController = navController)
+    composable<ReminderListScreen> {
+      ReminderListScreen(navController = navController)
     }
-
-    composable(
-      route = NavRoutes.MAP_SCREEN_BASE, arguments = listOf(
-      navArgument(NavRoutes.REMINDER_ID_KEY) {
-        type = NavType.IntType
-        defaultValue = -1
-      }
-    )) { backStackEntry ->
-      val reminderId = backStackEntry.arguments?.getInt(NavRoutes.REMINDER_ID_KEY)
-      MapBoxScreen(mapBoxScreenViewmodel, navController, reminderId)
-
+    composable<MapBoxScreen> { backStackEntry ->
+      val mapBoxScreen: MapBoxScreen = backStackEntry.toRoute()
+      MapBoxScreen(navController =  navController, reminderId = mapBoxScreen.reminderId)
     }
-
-
   }
 
 }
--- app/src/main/java/com/example/locationreminder/navigation/NavRoutes.kt
+++ app/src/main/java/com/example/locationreminder/navigation/NavRoutes.kt
@@ -1,4 +1,10 @@
 package com.example.locationreminder.navigation
 
-class NavRoutes {
-}
+import kotlinx.serialization.Serializable
+
+@Serializable
+object ReminderListScreen
+
+@Serializable
+data class MapBoxScreen(val reminderId: Long?)
+
