bfa9c21f5ab56d8e0ac5f10faf10f8e60484a745
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-11-14 04:21:24 +0100

    updating logic of handeling geoFence Transition

--- app/src/main/java/com/example/locationreminder/locationreminder/domain/usecases/HandleGeofenceTransitionUseCase.kt
+++ app/src/main/java/com/example/locationreminder/locationreminder/domain/usecases/HandleGeofenceTransitionUseCase.kt
@@ -1,19 +1,29 @@
 package com.example.locationreminder.locationreminder.domain.usecases
 
+import android.util.Log
+import com.example.locationreminder.locationreminder.data.service.GeofenceAppSource
 import com.example.locationreminder.locationreminder.domain.NotificationService
 import com.example.locationreminder.locationreminder.domain.model.Reminder
 import com.example.locationreminder.locationreminder.domain.repository.ReminderRepository
+import kotlinx.coroutines.flow.first
 
 class HandleGeofenceTransitionUseCase(
   private val reminderRepository: ReminderRepository,
+  private val geofenceSource: GeofenceAppSource,
   private val notificationService: NotificationService
 ) {
   suspend fun execute(featureId: String) {
     val reminderId = featureId.toIntOrNull() ?: return
-    val reminder: Reminder = reminderRepository.getReminderById(reminderId)
-    val updatedReminder = reminder.copy(isActive = false)
-    reminderRepository.saveReminder(updatedReminder)
-
+    val reminder = reminderRepository.getReminderById(reminderId)
+    if (!reminder.isActive) {
+      geofenceSource.removeFeature(featureId)
+      return
+    }
     notificationService.showReminderNotification(reminder)
+
+    reminderRepository.updateReminderStatus(reminderId, false)
+
+    geofenceSource.removeFeature(featureId)
+
   }
 }
